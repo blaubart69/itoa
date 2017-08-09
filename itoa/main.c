9991ce204426d00d73341c4260891fafdc9615a0
Stefan Kaschik <stefan.kaschik@unicredit.at>
2017-08-09 15:21:03 +0200

    more parameter checking added print of usage

--- itoa/main.c
+++ itoa/main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-	/*
-	printf("number of parameters: %d\n", argc);
+void printUsage(const char* programname);
 
-	for (int i = 0; i < argc; i++) {
-		printf("param# %d = %s\n", i, argv[i]);
-	}
-	*/
+int main(int argc, char *argv[]) {
 
 	if (argc < 2) {
-		fprintf(stderr, "Wasted: mission failed");
+		fprintf(stderr, "please enter a filename as first parameter\n");
+		printUsage(argv[0]);
+		return 1;
+	}
+
+	if (argc > 3) {
+		fprintf(stderr, "to many parameters given\n", argv[3]);
+		printUsage(argv[0]);
 		return 1;
 	}
 
 	char* conversion;
 	if (argc == 3) {
-		fprintf(stderr, "sie haben auch einen 2. parameter uebergeben: %s\n", argv[2]);
+		fprintf(stderr, "you have entered a 2nd parameter: %s\n", argv[2]);
 		conversion = argv[2];
 	}
 	else {
@@ -24,6 +26,10 @@ int main(int argc, char *argv[]) {
 	}
 
 	FILE *fileToPrint = fopen(argv[1], "rb");
+	if (fileToPrint == NULL) {
+		fprintf(stderr, "filename does not exist");
+		return 2;
+	}
 
 	char formatString[32];
 	sprintf(formatString, "%s\n", conversion);
@@ -37,4 +43,10 @@ int main(int argc, char *argv[]) {
 	fclose(fileToPrint);
 
 	return 0;
+}
+
+void printUsage(const char* programname) {
+	fprintf(stderr, "usage: %s filename [format]\n", programname);
+	fprintf(stderr, "   format ... printf format specifiers. sample: %%02X, %%d, ...\n");
+	fprintf(stderr, "   format ... printf format specifiers. sample: °/o02X, °/od, ...\n");
 }
