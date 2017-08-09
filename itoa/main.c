f842189878c9aa76f4cb4165ff67d4968989302d
E992661 <stefan.kaschik@unicredit.at>
2017-08-09 13:52:30 +0200

    a second parameter takes the formatstring

--- itoa/main.c
+++ itoa/main.c
@@ -10,15 +10,28 @@ int main(int argc, char *argv[]) {
 	*/
 
 	if (argc < 2) {
-		fprintf(stderr, "please specify a filename");
+		fprintf(stderr, "Wasted: mission failed");
 		return 1;
+	}
+
+	char* conversion;
+	if (argc == 3) {
+		fprintf(stderr, "sie haben auch einen 2. parameter uebergeben: %s\n", argv[2]);
+		conversion = argv[2];
+	}
+	else {
+		conversion = "%d";
 	}
 
 	FILE *fileToPrint = fopen(argv[1], "rb");
 
+	char formatString[32];
+	sprintf(formatString, "%s\n", conversion);
+
 	int oneByte;
 	while ( (oneByte = fgetc(fileToPrint)) != EOF) {
-		printf("%d\n", oneByte);
+		//printf("%d\n", oneByte);
+		printf(formatString, oneByte);
 	}
 
 	fclose(fileToPrint);
