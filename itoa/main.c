1bda035cd7328c8ba3e97a407fd012a13de4f614
Bernhard Spindler <bernhard.spindler@unicredit.at>
2017-08-09 11:26:25 +0200

    read the file as binary

--- itoa/main.c
+++ itoa/main.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+	/*
 	printf("number of parameters: %d\n", argc);
 
 	for (int i = 0; i < argc; i++) {
 		printf("param# %d = %s\n", i, argv[i]);
 	}
+	*/
 
 	if (argc < 2) {
 		fprintf(stderr, "please specify a filename");
 		return 1;
 	}
 
-	FILE *fileToPrint = fopen(argv[1], "r");
+	FILE *fileToPrint = fopen(argv[1], "rb");
 
 	int oneByte;
-	while ( (oneByte = fgetc(fileToPrint)) != -1) {
+	while ( (oneByte = fgetc(fileToPrint)) != EOF) {
 		printf("%d\n", oneByte);
 	}
 
