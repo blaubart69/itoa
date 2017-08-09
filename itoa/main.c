d73eb3d2eb1bcff4cf0dd4ea23a7779f9934d849
Bernhard Spindler <bernhard.spindler@unicredit.at>
2017-08-09 11:05:28 +0200

    initial

---
+++ itoa/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	printf("number of parameters: %d\n", argc);
+
+	for (int i = 0; i < argc; i++) {
+		printf("param# %d = %s\n", i, argv[i]);
+	}
+
+	if (argc < 2) {
+		fprintf(stderr, "please specify a filename");
+		return 1;
+	}
+
+	FILE *fileToPrint = fopen(argv[1], "r");
+
+	int oneByte;
+	while ( (oneByte = fgetc(fileToPrint)) != -1) {
+		printf("%d\n", oneByte);
+	}
+
+	fclose(fileToPrint);
+
+	return 0;
+}
